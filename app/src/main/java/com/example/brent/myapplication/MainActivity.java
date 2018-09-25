75a98abc318724ebc050450b7870a6cc21c0d3f1
brent <brent.willems.bw@gmail.com>
2018-09-25 19:28:40 +0200

    added dependency ButterKnife, also created simple textview that counts up on button press

--- app/src/main/java/com/example/brent/myapplication/MainActivity.java
+++ app/src/main/java/com/example/brent/myapplication/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.brent.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count=0;
+    @BindView(R.id.myButton) Button button;
+    @BindView(R.id.myTextView) TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.myButton)
+    public void addCount(){
+        textview.setText(String.valueOf(++count));
     }
 }
