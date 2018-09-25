00ae534b4acea5ea4406d78f5211e6057106b10a
brent <brent.willems.bw@gmail.com>
2018-09-25 19:40:17 +0200

    removed not used imports

--- app/src/main/java/com/example/brent/myapplication/MainActivity.java
+++ app/src/main/java/com/example/brent/myapplication/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.brent.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
