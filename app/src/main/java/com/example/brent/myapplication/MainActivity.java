eba46b7b25caa81f87eef8f18207b33db8d8105f
brent <brent.willems.bw@gmail.com>
2018-09-28 23:00:10 +0200

    added loginScreen with background and ability to save username in SharedPref

--- app/src/main/java/com/example/brent/myapplication/MainActivity.java
+++ app/src/main/java/com/example/brent/myapplication/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.brent.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,9 +18,11 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int count=0;
-    @BindView(R.id.myButton) Button button;
-    @BindView(R.id.myTextView) TextView textview;
+//    private int count=0;
+//    @BindView(R.id.myButton) Button button;
+//    @BindView(R.id.myTextView) TextView textview;
+    @BindView(R.id.usernameInput) EditText usernameInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +30,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = "";
+        String username = sharedPref.getString(("username"), defaultValue);
+
+        usernameInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    saveUsername();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        if(username!=""){
+            //TODO open new Activity or Fragment
+            Toast.makeText(getApplicationContext(),
+                    username,
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 
-    @OnClick(R.id.myButton)
-    public void addCount(){
-        textview.setText(String.valueOf(++count));
+
+    public void saveUsername(){
+        String username = usernameInput.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(),
+                username,
+                Toast.LENGTH_SHORT);
+
+        toast.show();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username",username);
+        editor.commit();
+
     }
 }
